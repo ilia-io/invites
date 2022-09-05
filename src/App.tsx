a0db818bb22680c915fe3f5d085293ef35b068bf
ilia-io <likeapu@gmail.com>
2022-09-05 19:26:03 +0300

    feat: Users component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Users from './components/Users';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://reqres.in/api/users`)
+      .then((res) => res.json())
+      .then((json) => setUsers(json.data))
+      .catch((error) => {
+        console.error(error);
+        alert('Ошибка при получении пользователей');
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Users items={users} isLoading={isLoading} />
     </div>
   );
 }
