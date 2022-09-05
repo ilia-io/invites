e17eed85767b319dd3e6fd826456cff1a9f25e90
ilia-io <likeapu@gmail.com>
2022-09-05 19:38:12 +0300

    feat: search

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Users from './components/Users';
 
 function App() {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState('');
 
   useEffect(() => {
     fetch(`https://reqres.in/api/users`)
@@ -16,9 +17,18 @@ function App() {
       .finally(() => setIsLoading(false));
   }, []);
 
+const onChangeSearchValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+  setSearchValue(event.target.value);
+};
+
   return (
     <div className="App">
-      <Users items={users} isLoading={isLoading} />
+      <Users
+        items={users}
+        isLoading={isLoading}
+        searchValue={searchValue}
+        onChangeSearchValue={onChangeSearchValue}
+      />
     </div>
   );
 }
